590f379aa403b3cc45a90bcc36ae1c985169aa8d
Pavila98 <37369661+Pavila98@users.noreply.github.com>
2018-05-01 04:26:15 +0000

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,10 @@
-x=int(input())
-temp=x
+y=int(input())
+temp=y
 rev=0
-while(x>0):
-    a=x%10
+while(y>0):
+    a=y%10
     rev=rev*10+a
-    x=int(x/10)
+    y=int(y/10)
 if temp==rev:
     print("it is a palindrome")
 else:
